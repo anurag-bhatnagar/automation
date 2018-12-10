689c0a47d589a21c14a6c84e547e0bf9a5cc1175
anurag-bhatnagar <30683399+anurag-bhatnagar@users.noreply.github.com>
2018-12-10 21:51:10 +0530

    Create controller.py

---
+++ engines/controller.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python
+
+import os
+########################################################################
+
+#---------------------------------ENGINE START-------------------------------------
+def engine_start():
+
+#---------------------------------ENGINE RUN-------------------------------------
+def engine_run():
+
+#---------------------------------ENGINE STOP-------------------------------------
+def engine_stop():
+
+#---------------------------------MAIN-------------------------------------
+def main():
+    # Step 1: Get engine (Steam|Diesel|Electric) from rail yard
+    
+    # Step 2: Attach bogies (Passenger|Containers)
+
+    # Step 3: Start engine (Green Flag)
+
+    # Step 4: Run engine
+
+    # Step 5: Wait for journey to be completed
+    
+    # Step 6: Stop engine
+    
+    # Step 7: Unload bogies
+    
+    #Step 8: Send engine to rail yard
+
+
+#---------------------------------INIT-------------------------------------
+if __name__ == "__main__":
+    
+  
+    main()
+
+# End of File
