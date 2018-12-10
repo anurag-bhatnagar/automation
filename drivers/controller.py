fa100f0d11bf1b1e23ddd2d015c8d2a0e773d862
anurag-bhatnagar <30683399+anurag-bhatnagar@users.noreply.github.com>
2018-12-10 21:59:34 +0530

    Create controller.py

---
+++ drivers/controller.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import os
+########################################################################
+
+#---------------------------------DRIVERS SELECTED-------------------------------------
+def drivers_selected():
+
+#---------------------------------DRIVERS ROUTE INFO-------------------------------------
+def drivers_route_info():
+
+#---------------------------------DRIVERS RELEASE-------------------------------------
+def drivers_release():
+
+#---------------------------------MAIN-------------------------------------
+def main():
+    # Step 1: Select drivers for train journey
+    
+    # Step 2: Brief drivers about the train route 
+
+    # Step 3: Handover the travel ledgers to drivers
+
+    # Step 4: Release drivers
+
+#---------------------------------INIT-------------------------------------
+if __name__ == "__main__":
+    
+  
+    main()
+
+# End of File
